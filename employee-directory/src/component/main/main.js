240eb0fc43c4d2b02c8d68928ad6ef2f1a225c74
Dylan Frank <drfrank22@gmail.com>
2021-02-02 14:35:29 -0500

    created main component

---
+++ employee-directory/src/component/main/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "../dataArea/dataArea";
+import "./main.css";
+
+function Main() {
+    return (
+        <div>
+            <DataArea />
+        </div>
+    );
+};
+
+export default Main;
