9b0118e2eafbe4f14f17e4e72a0f356add3cbfed
Dylan Frank <drfrank22@gmail.com>
2021-02-02 15:27:54 -0500

    fixed API file

--- src/util/api.js
+++ src/util/api.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 export default {
-
-    getEmployees: function() {
-
-        return axios.get("https://randomuser.me/api/?results=100&nat=us");
-
-    }
     
+  getEmployees: function() {
+
+    return axios.get("https://randomuser.me/api/?results=100&nat=us");
+  }
 };
